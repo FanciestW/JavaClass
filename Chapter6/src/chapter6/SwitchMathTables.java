092b0066b230ba6a951ba8c2b366aa9c8510b17f
FanciestW <wlin26@yahoo.com>
2015-03-02 16:14:57 -0500

    Finished exercise 6.3

--- Chapter6/src/chapter6/SwitchMathTables.java
+++ Chapter6/src/chapter6/SwitchMathTables.java
@@ -1,11 +1,13 @@
 /*
 MathTables.java
 By: William Lin
-This program outputs 3 different tables. One for the common log. One for addition.
-And one for sin, cos and tan. For the common log table it will print a table for
+This program outputs 5 different tables. One for the common log. One for addition. One for sin,
+one for cos and one for tan. For the common log table it will print a table for
 common logs from 0 to 10 with increments of 0.5. For the addition table it will print a 2 dimensional
-table for the numbers 0 to 12. And for the trig table. It will print the cos, sin and tan of angles
-between 0 and 360 with increments of 30 degrees. In this program you choose which table you want displayed.
+table for the numbers 0 to 12. printSinTable will print the sin of the angles
+between 0 and 360 with increments of 30 degrees. printCosTable will print the cos of the angles
+between 0 and 360 with increments of 30 degrees. printTanTable will print the tan of the angles
+between 0 and 360 with increments of 30 degrees.In this program you choose which table you want displayed.
 */
 
 package chapter6;
@@ -14,7 +16,56 @@ import java.util.Scanner;
 public class SwitchMathTables{
     
     public static void main(String[] args){
-        
+        System.out.println("1. Common Log Table\n2. Addition Table\n3. Sin Table\n4. Cos Table\n5. Tan Table\n6. All Tables");
+        System.out.print("Your Choice(1-6): ");
+        Scanner scChoice = new Scanner(System.in);
+        int choice = scChoice.nextInt();
+        System.out.println();
+        switch(choice){
+            case 1:
+                printCommonLogTable();
+                break;
+            case 2:
+                printAdditionTable();
+                break;
+            case 3:
+                printSinTable();
+                break;
+            case 4:
+                printCosTable();
+                break;
+            case 5:
+                printTanTable();
+                break;
+            case 6:
+                printCommonLogTable();
+                System.out.println();
+                printAdditionTable();
+                System.out.println();
+                printSinTable();
+                System.out.println();
+                printCosTable();
+                System.out.println();
+                printTanTable();
+                break;
+            default:
+                System.out.println("Your choice is invalid. Please try again.");
+                break;
+        }
+    }
+    
+    /**
+     * This method prints a table of common logs from 0 to 10
+     */
+    public static void printCommonLogTable(){
+        System.out.println("Common Log Table:");
+        double i = 0;
+        while(i <= 10){
+            //double log = Math.log10(i); This can be in place of the conversion on line 26.
+            double log = (Math.log(i)/Math.log(10));
+            System.out.println(i + ":  " + log);
+            i = i + 0.5;
+        }
     }
     
     /**
@@ -43,47 +94,38 @@ public class SwitchMathTables{
         System.out.println();
     }
     /**
-     * This method prints a table for the sin, cos, and tan of the angles between 0 and 360.
+     * This method prints a table for the sin of the angles between 0 and 360.
      * Increasing by increments of 30 degrees each time for each.
      */
-    public static void printTrigTables(){
-        System.out.println("Trig Table:");
+    public static void printSinTable(){
+        System.out.println("Sin Table:");
         for(double i = 0; i <= 360; i = i + 30){
-            System.out.println(i + ": " + sin(i) + "..." + cos(i) + "..." + tan(i));
+            double rad = (i * Math.PI)/180;
+            System.out.println(i + ": " + Math.sin(rad));
         } 
-   }
-    
-    /**
-     * This method helps the printTrigTable() method by finding the sin of 
-     * the parameter n.
-     * @param deg The degree used to find sin. Converted to radians.
-     * @return The sin of parameter n.
-     */
-    public static double sin(double deg){
-        double rad = (deg * Math.PI)/180;
-        return(Math.sin(rad));
     }
     
     /**
-     * This method helps the printTrigTable() method by finding the cos of 
-     * the parameter n.
-     * @param deg The degree used to find cos. Converted to radians.
-     * @return The cos of parameter n.
+     * This method prints a table for the cos of the angles between 0 and 360.
+     * Increasing by increments of 30 degrees each time for each.
      */
-    public static double cos(double deg){
-        double rad = (deg * Math.PI)/180;
-        return(Math.cos(rad));
+    public static void printCosTable(){
+        System.out.println("Cos Table:");
+        for(double i = 0; i <= 360; i = i + 30){
+            double rad = (i * Math.PI)/180;
+            System.out.println(i + ": " + Math.cos(rad));
+        } 
     }
     
-    
     /**
-     * This method helps the printTrigTable() method by finding the tan of 
-     * the parameter n.
-     * @param deg The degree used to find tan. Converted to radians.
-     * @return The tan of parameter n.
+     * This method prints a table for the tan of the angles between 0 and 360.
+     * Increasing by increments of 30 degrees each time for each.
      */
-    public static double tan(double deg){
-        double rad = (deg * Math.PI)/180;
-        return(Math.tan(rad));
+    public static void printTanTable(){
+        System.out.println("Tan Table:");
+        for(double i = 0; i <= 360; i = i + 30){
+            double rad = (i * Math.PI)/180;
+            System.out.println(i + ": " + Math.tan(rad));
+        } 
     }
 }
