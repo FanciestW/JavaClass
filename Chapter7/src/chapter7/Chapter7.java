e2a410a3138026daa78382e5d9d9d06eff03c169
FanciestW <wlin26@yahoo.com>
2015-03-04 17:43:58 -0500

    Started chapter 7 week 6

---
+++ Chapter7/src/chapter7/Chapter7.java
@@ -0,0 +1,19 @@
+package chapter7;
+import java.applet.Applet;  //Imports the Applet Class
+import java.awt.Graphics;   //Imports the Graphics class, used to draw lines, circles, squares, text, etc.
+
+/**
+ * This class implements an applet that simply displays "Hello World"
+ * @author William
+ */
+public class Chapter7 extends Applet{
+    
+    public void paint(Graphics g){
+        /*drawString is a method of the Graphics class. It takes a string,
+        and two integer parameters. As x and y coordinates. These coordinates 
+        correspond to Quadrant I of a traditional coordinate plane, 
+        so they are always positive.
+        */
+        g.drawString("Hello World Applet", 500, 500);
+    }
+}
