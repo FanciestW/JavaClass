56244119fe4f2fb569ce6a692243e35241b364d1
FanciestW <wlin26@yahoo.com>
2015-03-09 22:03:29 -0400

    Made changes

---
+++ Applet/myApplet.java
@@ -0,0 +1,9 @@
+import javax.swing.JApplet;
+import java.awt.Graphics;
+
+public class myApplet extends JApplet{
+	
+	public void paint(Graphics g){
+		g.drawString("William Lin", 240, 240);
+	}
+}
---
+++ Chapter7/src/chapter7/Mystery.java
@@ -0,0 +1,18 @@
+package chapter7;
+public class Mystery {
+ 	public static String bing (String s) {
+		int i = s.length() - 1;
+	 	String total = "";
+	 	while (i >= 0 ) {
+		 	char ch = s.charAt (i);
+		 	System.out.println (i + " " + ch);
+		 	total = total + ch;
+		 	i--;
+	 	}
+	 	return total;
+	}
+
+	public static void main (String[] args) {
+ 		System.out.println (bing ("Allen"));
+ 	}
+} 
