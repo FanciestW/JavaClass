d62a3aac29f852968448c234e62afe3dbfce5e61
William Lin <wlin26@yahoo.com>
2015-04-21 13:59:54 -0400

    New tester with Game.java

--- Cards/src/Game.java
+++ Cards/src/Game.java
@@ -1,10 +1,11 @@
 public class Game{
     
     public static void main(String[] args){
-        Card one = new Card(0, 1);
-        Card[] deck = {one};
-        int[] suithist = Card.suitHist(deck);
-        int[] rankhist = Card.rankHist(deck);
-        Card.printArray(rankhist);
+        Card one = new Card(0, 2);
+        Card two = new Card(0, 2);
+        Card three = new Card(0, 4);
+        Card four = new Card(2, 2);
+        Card[] hand = {one, two, three, four};
+        System.out.println(Card.isThreeKind(hand));
     }
 }
