608a8c0a4578cce93363fc52d03d1799ad1f10d6
FanciestW <wlin26@yahoo.com>
2015-04-21 22:29:05 -0400

    Changes

--- Cards/src/Game.java
+++ Cards/src/Game.java
@@ -4,15 +4,19 @@ By: William Lin
 This program utilizes the Card class to create Cards and Decks and Hands. And it utilizes
 the methods in the Card class to create histograms and check you hand or deck.
 */
+import java.util.Random;
 
 public class Game{
     
     public static void main(String[] args){
+        //4 Randomly generated cards for your hand.
+        Random random = new Random();
         Card[] deck = Card.buildDeck();
-        Card one = new Card(0, 2);
-        Card two = new Card(0, 2);
-        Card three = new Card(0, 4);
-        Card four = new Card(0, 2);
+        Card one = new Card(random.nextInt(4), random.nextInt(13) + 1);
+        Card two = new Card(random.nextInt(4), random.nextInt(13) + 1);
+        Card three = new Card(random.nextInt(4), random.nextInt(13) + 1);
+        Card four = new Card(random.nextInt(4), random.nextInt(13) + 1);
+        //One set of card in your hand is set though.
         Card five = Card.parseCard("Queen of Clubs");
         Card[] hand = {one, two, three, four, five};
         System.out.println("Your Hand: ");
